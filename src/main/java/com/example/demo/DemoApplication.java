c3316986dbde1b89f90232b11ddcec6f5e35a7b5
Raya <rayameha@gmail.com>
2017-07-11 16:21:21 -0400

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import it.ozimov.springboot.mail.configuration.EnableEmailTools;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEmailTools
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
